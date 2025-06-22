3bf11efb5caf3fbdcbfc263ddcf9345584d768c0
romeoonyeka <romeoonyeka@gmail.com>
2025-06-22 10:41:28 +0000

    vitest import is removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
